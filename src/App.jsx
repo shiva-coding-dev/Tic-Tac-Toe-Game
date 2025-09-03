11d7bc3b13eb054df3ad464f07c15eea68099ca1
Shiva Gupta <shivaiitpatna@gmail.com>
2025-09-04 01:28:21 +0530

    Add the functionality of the player activeness

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { act, useState } from "react";
 import Player from "./components/Player" ;
 import GameBoard from "./components/GameBoard";
 function App() {
+  const [activePlayer , setActivePlayer] = useState('X');
+
+  function handleActivePlayer(){
+    setActivePlayer((currActive) => currActive === 'X' ?'O':'X');
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName = "player 1" symbol = "X"/>
-          <Player initialName = "player 2" symbol = "O"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName = "player 1" symbol = "X" isActive={activePlayer === 'X' }/>
+          <Player initialName = "player 2" symbol = "O" isActive={activePlayer === 'O' }/>
         </ol>
-        <GameBoard/>
+        <GameBoard onSelectSquare={handleActivePlayer} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   )
